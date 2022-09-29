85bb6e41f09fff0fae17042a2868d95b1bd4b5e1
ashishgupta544 <mayankg1209@gmail.com>
2022-09-29 16:32:23 +0530

    accounts app

--- social_site/urls.py
+++ social_site/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from re import template
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.views.generic import TemplateView
+from accounts import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',TemplateView.as_view(template_name='index.html'),name='home'),
+    path('', include(urls)),
 ]
